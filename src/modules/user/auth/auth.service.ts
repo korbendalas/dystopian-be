13d0fed2e14d65678d37a0d72d980e055f7ce96e
Dalibor <daliborpetric99@gmail.com>
2022-12-27 11:45:47 +0100

    Create new user and hash password

--- src/modules/user/auth/auth.service.ts
+++ src/modules/user/auth/auth.service.ts
@@ -1,17 +1,31 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { SignupDto } from '../dtos/auth.dto';
+import * as bcrypt from 'bcryptjs';
+import { UserType } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
-  async signup({ email, ...data }: SignupDto) {
-    const user = await this.prismaService.user.count();
+  async signup({ email, password, name, telephone }: SignupDto) {
+    const user = await this.prismaService.user.findUnique({ where: { email } });
 
     if (user) {
       throw new ConflictException();
     }
 
-    return data;
+    const hash = await bcrypt.hash(password, 10);
+
+    const newUser = await this.prismaService.user.create({
+      data: {
+        email,
+        password: hash,
+        name,
+        telephone,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    return newUser;
   }
 }
