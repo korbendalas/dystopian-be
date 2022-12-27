53b52fa73c44d1e350ab761d4ce1085381bbebee
Dalibor <daliborpetric99@gmail.com>
2022-12-27 12:03:15 +0100

    generate token

--- src/modules/user/auth/auth.service.ts
+++ src/modules/user/auth/auth.service.ts
@@ -3,7 +3,8 @@ import { PrismaService } from '../../prisma/prisma.service';
 import { SignupDto } from '../dtos/auth.dto';
 import * as bcrypt from 'bcryptjs';
 import { UserType } from '@prisma/client';
-
+import * as jwt from 'jsonwebtoken';
+import * as process from 'process';
 @Injectable()
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
@@ -26,6 +27,10 @@ export class AuthService {
       },
     });
 
-    return newUser;
+    const token = jwt.sign({ name, id: newUser.id }, process.env.JWT_SECRET, {
+      expiresIn: '5 days',
+    });
+
+    return { token };
   }
 }
