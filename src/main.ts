c94c1220c7d534a8e4b0bfc8c71e266c27b8b7bc
Dalibor <daliborpetric99@gmail.com>
2023-01-10 12:07:19 +0100

    update env-s

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const port = process.env.PORT || 4000;
+  console.log('ENV', process.env.NODE_ENV);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,6 +14,6 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
