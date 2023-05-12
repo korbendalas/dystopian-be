1e797c6ab1a91712b76302245c58075bb88df3eb
dalibor <daliborpetric99@gmail.com>
2023-05-12 20:42:09 +0200

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 4000;
-  console.log('ENV', process.env.NODE_ENV);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,6 +15,14 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Realtor API')
+    .setDescription('Realtor API description')
+    .setVersion('1.0')
+    .addTag('Realtor V1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(port);
 }
 bootstrap();
