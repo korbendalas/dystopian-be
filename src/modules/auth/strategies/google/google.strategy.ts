c0a1bd6f0445e546c3211869e5c8b086653ffea1
dalibor <daliborpetric99@gmail.com>
2023-05-29 12:20:29 +0200

    update changes to envs

--- src/modules/auth/strategies/google/google.strategy.ts
+++ src/modules/auth/strategies/google/google.strategy.ts
@@ -8,7 +8,10 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_REDIRECT_URL,
+      callbackURL:
+        process.env.NODE_ENV === 'development'
+          ? process.env.GOOGLE_REDIRECT_URL_DEV
+          : process.env.GOOGLE_REDIRECT_URL_PROD,
       passReqToCallback: true,
       scope: ['email', 'profile'],
     });
